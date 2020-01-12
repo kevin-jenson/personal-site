d390e0ac79cb4553da1c2e9637aa2ddfbc993186
Kevin Jenson <kjjenson@gmail.com>
2020-01-11 18:07:34 -0700

    modified layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,13 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { makeStyles } from "@material-ui/styles";
+import { DesktopHeader as Header } from "./Navigation";
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+const useStyles = makeStyles(theme => ({
+  appContainer: {
+    backgroundColor: theme.background,
+  },
+}));
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,32 +18,21 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
+
+  const classes = useStyles();
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+    <div className={classes.appContainer}>
+      <Header />
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  );
+};
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default Layout;
