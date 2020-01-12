59eb30fadd739752c96d59b3e7054d02eba9070c
Kevin Jenson <kjjenson@gmail.com>
2020-01-12 12:28:24 -0700

    adjusted theme

--- theme.js
+++ theme.js
@@ -1,4 +1,4 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const colors = {
   black: "#0A100D",
@@ -6,12 +6,18 @@ const colors = {
   gray: "#40434E",
   lightPink: "#A39BA8",
   pink: "#C297B8",
-}
+};
 
 const theme = createMuiTheme({
   colors,
-	background: colors.black,
-	spacer: 8
+  background: colors.black,
+  spacer: 8,
+  typography: {
+    h1: {
+      fontSize: 36,
+      fontWeight: 700,
+    },
+  },
 });
 
-export default theme
+export default theme;
