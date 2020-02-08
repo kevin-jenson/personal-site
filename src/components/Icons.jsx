f5f056e6a1220f43fa72da9364f8c112382429d6
Kevin Jenson <kjjenson@gmail.com>
2020-02-08 16:00:41 -0700

    added form icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -54,3 +54,42 @@ export const Cursor = React.forwardRef((props, ref) => {
 
   return <span ref={ref} className={classes.cursor} {...props} />;
 });
+
+export const Profile = () => {
+  const theme = useTheme();
+
+  return (
+    <svg style={{ width: 25, height: 25 }} viewBox="0 0 24 24">
+      <path
+        fill={theme.colors.white}
+        d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z"
+      />
+    </svg>
+  );
+};
+
+export const Mail = () => {
+  const theme = useTheme();
+
+  return (
+    <svg style={{ width: 25, height: 25 }} viewBox="0 0 24 24">
+      <path
+        fill={theme.colors.white}
+        d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z"
+      />
+    </svg>
+  );
+};
+
+export const Pencil = () => {
+  const theme = useTheme();
+
+  return (
+    <svg style={{ width: 25, height: 25 }} viewBox="0 0 24 24">
+      <path
+        fill={theme.colors.white}
+        d="M16.84,2.73C16.45,2.73 16.07,2.88 15.77,3.17L13.65,5.29L18.95,10.6L21.07,8.5C21.67,7.89 21.67,6.94 21.07,6.36L17.9,3.17C17.6,2.88 17.22,2.73 16.84,2.73M12.94,6L4.84,14.11L7.4,14.39L7.58,16.68L9.86,16.85L10.15,19.41L18.25,11.3M4.25,15.04L2.5,21.73L9.2,19.94L8.96,17.78L6.65,17.61L6.47,15.29"
+      />
+    </svg>
+  );
+};
