0a9ee4a23088388f461111c89a04695dc4751e3b
Kevin Jenson <kjjenson@gmail.com>
2020-02-09 16:24:46 -0700

    working on contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,6 +4,7 @@ import SEO from "../components/Seo";
 
 import { makeStyles } from "@material-ui/styles";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 import { TextField, Button } from "../components/Controls";
 import { Mail, Pencil, Profile } from "../components/Icons";
@@ -18,13 +19,17 @@ const useStyles = makeStyles(theme => ({
   buttonContainer: {
     marginTop: theme.spacer * 4,
   },
+  form: {
+    marginTop: theme.spacer * 8,
+  },
 }));
 
 const Contact = () => {
   const classes = useStyles();
-  const [{ label, Icon }, setCurrentInputProps] = React.useState({
+  const [{ label, Icon, value }, setCurrentInputProps] = React.useState({
     label: "What's your name?",
     Icon: Profile,
+    value: "name",
   });
 
   const [formState, setFormState] = React.useState({
@@ -33,38 +38,105 @@ const Contact = () => {
     message: "",
   });
 
+  const handleOnInput = ({ target }) => {
+    setFormState({ ...formState, [value]: target.value });
+  };
+
+  const handleNext = event => {
+    event.preventDefault();
+
+    switch (value) {
+      case "name":
+        setCurrentInputProps({
+          label: "What's your email?",
+          Icon: Mail,
+          value: "email",
+        });
+        break;
+      case "email":
+        setCurrentInputProps({
+          label: "Write me a message.",
+          Icon: Pencil,
+          value: "message",
+        });
+        break;
+      case "message":
+        break;
+      default:
+        throw new Error(`case not found: ${value}`);
+    }
+  };
+
+  const handleSend = event => {
+    event.preventDefault();
+
+    setFormState({
+      name: "",
+      email: "",
+      message: "",
+    });
+
+    alert("sent!");
+  };
+
+  const allFormStateFilled =
+    Object.values(formState).filter(Boolean).length === 3;
+
   return (
     <Layout>
       <SEO title="Contact Me" />
       <div className={classes.container}>
-        <h1>Shoot me an Email!</h1>
-        <p>
+        <Typography variant="h5" component="h1">
+          Shoot me an Email!
+        </Typography>
+        <Typography variant="body1" component="p">
           Let me know about any projects you wanna collaborate on, any
           opportunities you wanna talk to me about, or if you just wanna say
           whats up!
-        </p>
+        </Typography>
         <p>Fill out the form below or email me at kjjenson@gmail.com.</p>
-        <Grid container spacing={0} alignItems="flex-end">
-          <Grid item xs={1}>
-            <Icon />
-          </Grid>
-          <Grid item xs={11}>
-            <TextField label={label} />
-          </Grid>
-        </Grid>
-        <Grid
-          container
-          spacing={1}
-          justify="flex-start"
-          className={classes.buttonContainer}
+        <form
+          className={classes.form}
+          onSubmit={allFormStateFilled ? handleSend : handleNext}
         >
-          <Grid item xs={2}>
-            <Button>Next</Button>
+          <Grid container spacing={0} alignItems="flex-end">
+            <Grid item xs={1}>
+              <Icon />
+            </Grid>
+            <Grid item xs={11}>
+              <TextField
+                label={label}
+                onInput={handleOnInput}
+                value={formState[value]}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={2}>
-            <Button>Submit</Button>
+          <Grid
+            container
+            spacing={1}
+            justify="flex-start"
+            className={classes.buttonContainer}
+          >
+            <Grid item xs={2}>
+              <Button
+                disabled={!Boolean(formState[value]) || allFormStateFilled}
+                onClick={handleNext}
+                type={allFormStateFilled ? "button" : "submit"}
+              >
+                Next
+              </Button>
+            </Grid>
+            <Grid item xs={2}>
+              <Button
+                disabled={!allFormStateFilled}
+                onClick={handleSend}
+                type={allFormStateFilled ? "submit" : "button"}
+              >
+                Send
+              </Button>
+            </Grid>
           </Grid>
-        </Grid>
+        </form>
       </div>
     </Layout>
   );
